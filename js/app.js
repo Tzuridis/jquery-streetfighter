5610a3d7472d5e29df6473164a9a008032dd20ff
Ted Tzuridis <tzuridis@gmail.com>
2016-07-06 16:57:01 -0400

    Just need to add one guy

--- js/app.js
+++ js/app.js
@@ -2,15 +2,20 @@ $(document).ready(function() {
     $('.ryu').mouseenter(function() {
         $('.ryu-still').hide();
         $('.ryu-ready').show();
+        $('.ryu-throwing').hide();
+        $('.cool').hide();
     })
         .mouseleave(function() {
             $('.ryu-ready').hide();
             $('.ryu-still').show();
+            $('.ryu-throwing').hide();
+            $('.cool').hide();
         })
         .mousedown(function() {
             playHadouken();
             $('.ryu-ready').hide();
             $('.ryu-throwing').show();
+            $('.ryu-still').hide()
             $('.hadouken').finish().show()
                 .animate({
                         'left': '1020px'
@@ -26,7 +31,11 @@ $(document).ready(function() {
             console.log('mouseup')
             $('.ryu-throwing').hide();
             $('.ryu-ready').show();
-        });
+            $('.cool').hide();
+            $('.ryu-still').hide();
+        })
+
+
 });
 
 function playHadouken() {
@@ -34,3 +43,22 @@ function playHadouken() {
     $('#hadouken-sound')[0].load();
     $('#hadouken-sound')[0].play();
 }
+
+$(document).keydown(function(event) {
+    if (event.which === 88) {
+        console.log("Keydown")
+        $('.ryu-ready').hide()
+        $('.cool').show()
+        $('.ryu-still').hide();
+        $('.ryu-throwing').hide();
+    }
+})
+    .keyup(function(event) {
+        if (event.which === 88) {
+            console.log("Keyup")
+            $('.ryu-ready').hide();
+            $('.cool').hide();
+            $('.ryu-still').show();
+            $('.ryu-throwing').hide();
+        }
+    })
