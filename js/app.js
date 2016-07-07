040fe34a134efe9e6a37bf868f8621b675b232a5
Ted Tzuridis <tzuridis@gmail.com>
2016-07-07 14:41:27 -0400

    Complete

--- js/app.js
+++ js/app.js
@@ -11,7 +11,8 @@ $(document).ready(function() {
             $('.ryu-throwing').hide();
             $('.cool').hide();
         })
-        .mousedown(function() {
+        .mousedown(function(event) {
+            if (event.which === 1) {
             playHadouken();
             $('.ryu-ready').hide();
             $('.ryu-throwing').show();
@@ -25,14 +26,16 @@ $(document).ready(function() {
                         $(this).hide();
                         $(this).css('left', '520px');
                     }
-            );
+            )};
         })
-        .mouseup(function() {
+        .mouseup(function(event) {
+            if (event.which === 1) {
             console.log('mouseup')
             $('.ryu-throwing').hide();
             $('.ryu-ready').show();
             $('.cool').hide();
             $('.ryu-still').hide();
+        }
         })
 
 
@@ -60,5 +63,75 @@ $(document).keydown(function(event) {
             $('.cool').hide();
             $('.ryu-still').show();
             $('.ryu-throwing').hide();
+        }
+    })
+
+    // Second RYU
+
+    $(document).ready(function() {
+    $('.ryu2').mouseenter(function() {
+        $('.ryu2-still').hide();
+        $('.ryu2-ready').show();
+        $('.ryu2-throwing').hide();
+        $('.cool2').hide();
+    })
+        .mouseleave(function() {
+            $('.ryu2-ready').hide();
+            $('.ryu2-still').show();
+            $('.ryu2-throwing').hide();
+            $('.cool2').hide();
+        })
+        .mousedown(function(event) {
+            if (event.which === 2) {
+            playHadouken();
+            $('.ryu2-ready').hide();
+            $('.ryu2-throwing').show();
+            $('.ryu2-still').hide()
+            $('.hadouken2').finish().show()
+                .animate({
+                        'left': '1020px'
+                    },
+                    500,
+                    function() {
+                        $(this).hide();
+                        $(this).css('left', '520px');
+                    }
+            )};
+        })
+        .mouseup(function(event) {
+            if (event.which === 2) {
+            console.log('mouseup')
+            $('.ryu2-throwing').hide();
+            $('.ryu2-ready').show();
+            $('.cool2').hide();
+            $('.ryu2-still').hide();
+        }
+        })
+
+
+});
+
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
+
+$(document).keydown(function(event) {
+    if (event.which === 67) {
+        console.log("Keydown")
+        $('.ryu2-ready').hide()
+        $('.cool2').show()
+        $('.ryu2-still').hide();
+        $('.ryu2-throwing').hide();
+    }
+})
+    .keyup(function(event) {
+        if (event.which === 67) {
+            console.log("Keyup")
+            $('.ryu2-ready').hide();
+            $('.cool2').hide();
+            $('.ryu2-still').show();
+            $('.ryu2-throwing').hide();
         }
     })
